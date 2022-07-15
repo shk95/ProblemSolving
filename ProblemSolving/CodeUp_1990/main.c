020ce05999d5e544369e349fa14e64c815a9ba6c
user123145 <shk9510@gmail.com>
2022-07-15 13:44:03 +0900

    문제풀이 알고리즘 개선

--- ProblemSolving/CodeUp_1990/main.c
+++ ProblemSolving/CodeUp_1990/main.c
@@ -3,47 +3,34 @@
 //나눗셈 과정을 알고리즘화
 int main()
 {
-    //문자열 최대자릿수만큼 지정후 저장
+    /*문자열 최대자릿수만큼 지정후 저장
+        에서 문제 발생. 컴파일러종류에 따른 배열을 변수로 지정할 때의 에러.
+        제시된 10의 500제곱 크기의 배열 선언에서 문제가 생김.
+
+
+
     int maxSize = 1;
     for (int i = 0; i < 500; i++)
     {
         maxSize *= 10;
     }
-    char temp[maxSize];
+
+    */
+
+    char temp[100000000];
     scanf("%s", temp);
 
-    //문자열을 정수배열로 바꿈
-    int len = strlen(temp);
-    int num[len];
-    for (int i = 0; i < len; i++)
-    {
-        num[i] = temp[i] - 48;
-    }
+    //각자릿수를 전부 더하는 것으로 계산과정 단축
+    long long int sumNum = 0;
+    int i = 0;
 
-    /*
-    나누는수(3) 으로 빼기 반복. 3보다 작은수일때는 다음 자릿수로 값을 넘겨서 합침.
-    마지막 자릿수일때는 남은숫자가 나머지.
-    */
-    int res;
-    for (int i = 0; i < len; i++)
+    while (temp[i] != '\0')
     {
-        for (int j = 0; j < 3; j++)
-        {
-            if (num[i] < 3)
-            {
-                break;
-            }
-            num[i] -= 3;
-        }
-        if (num[i] == num[len - 1])
-        {
-            res = num[i];
-        }
-        else
-        {
-            num[i + 1] += num[i];
-        }
+        sumNum += temp[i] - 48;
+        i++;
     }
+    int res = sumNum % 3;
+
     if (res == 0)
     {
         printf("%d", 1);
