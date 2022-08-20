e235a2f8a788137ac6508516c32adc9f41d78b6d
user123145 <shk9510@gmail.com>
2022-08-20 22:24:57 +0900

    .

--- BaekJoon/01316/Main.java
+++ BaekJoon/01316/Main.java
@@ -1,32 +1,34 @@
+//각 문자열에서 연속된 단어 묶음이 중복되지 않는것만 카운트
 import java.util.Scanner;
 public class Main{
-    public static void main(String [] args){
+    public static void mai트(String [] args){
         Scanner sc = new Scanner(System.in);
         int Cnt = sc.nextInt();
-        String str;
+
         int resCnt = Cnt;
 
+        String str;
         for(int i = 0; i<Cnt; i++){
-            str = sc.next();
+            str = sc.next();//라인별 문자열
             int strLen = str.length();
 
-            char[] charArr = new char[strLen];
+            char[] charArr = new char[strLen];//문자열별 문자 나눔
             int charArrIdx = 0;      
 
             //문자 중복 검증
             charArr[0] = str.charAt(0);
             for(int j = 1; j<strLen; j++){
-                if(charArr[charArrIdx]!=str.charAt(j)){
+                if(charArr[charArrIdx]!=str.charAt(j)){//단어의 연속 중 다른 문자가 나왔을때
                     charArrIdx++;
-                    charArr[charArrIdx] = str.charAt(j);
+                    charArr[charArrIdx] = str.charAt(j);//새로운 단어로
                     
-                    int charCnt = 0;
+                    int charCnt = 0;//문자 배열중 중복된 단어 카운트
                     for(int k = 0; k<charArr.length; k++){
                         if(charArr[charArrIdx]==charArr[k]){
                             charCnt++;
                         }
                     }
-                    if(charCnt>1){
+                    if(charCnt>1){//중복 발견시 종료
                         resCnt--;
                         break;
                     }
@@ -36,3 +38,4 @@ public class Main{
         System.out.println(resCnt);
     }
 }
+//속도 메모리면에서 만족스러운 편이지만 조금더 개선 가능해보임.
