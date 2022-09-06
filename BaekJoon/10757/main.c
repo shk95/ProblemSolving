b94a80e98e08530950361668f2682219e60b3a42
user123145 <shk9510@gmail.com>
2022-09-06 20:13:23 +0900

    정답

--- BaekJoon/10757/main.c
+++ BaekJoon/10757/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
@@ -10,24 +11,52 @@ int main()
     lenA = strlen(A);
     lenB = strlen(B);
 
-    int res[10003];
-    int overTen = 0;
-    if (lenA >= lenB)
+    int numBigger;
+    if (lenA > lenB)
     {
-        int indexA = lenA - 1;
-        for (int i = lenB - 1; lenB < 0; i--)
+        numBigger = lenA;
+    }
+    else
+    {
+        numBigger = lenB;
+    }
+
+    //결과저장공간
+    int *resNum = calloc(numBigger + 1, sizeof(int));
+
+    //결과 resNum에 거꾸로 저장
+    int a = lenA - 1, b = lenB - 1;
+    for (int i = 0; i < lenA; i++)
+    {
+        resNum[i] = A[a] - '0';
+        a--;
+    }
+    for (int i = 0; i < lenB; i++)
+    {
+        resNum[i] += B[b] - '0';
+        b--;
+    }
+
+    //한 칸에서 10이상인 수 정리
+    for (int i = 0; i < numBigger; i++)
+    {
+        if (resNum[i] > 9)
         {
-            if (overTen == 1)
-            {
-                res[indexA] += 1;
-                overTen = 0;
-            }
-            if ((A[indexA] - '0') + (B[i] - '0') > 9)
-            {
-                overTen += 1;
-                res[indexA] += (A[indexA] - '0') + (B[i] - '0') - 10;
-            }
-            indexA--;
+            resNum[i + 1]++;
+            resNum[i] -= 10;
         }
     }
+
+    //출력
+    if (resNum[numBigger] > 0)
+    {
+        printf("%d", resNum[numBigger]);
+    }
+    for (int i = numBigger - 1; i > -1; i--)
+    {
+        printf("%d", resNum[i]);
+    }
 }
+/**
+ *  매우 큰 수 더하기 로직을 구현
+ */
