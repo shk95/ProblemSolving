2f4cee85a64db95f1b022e8b2272cd632fa8c6bc
user123145 <shk9510@gmail.com>
2022-09-08 11:32:03 +0900

    작성중

--- BaekJoon/02839/main.c
+++ BaekJoon/02839/main.c
@@ -2,20 +2,32 @@
 int main()
 {
     int N;
-    N = scnaf("%d", &N);
+    N = scanf("%d", &N);
 
-    int count = N / 5;
-    N = N % 5;
-    for (;;)
+    int check = 1;
+
+    float countThree;
+    int countFive = N / 5;
+    for (; countFive >= 0; countFive--)
     {
-        if ((N % 5) == 0)
+        countThree = countFive * 5 * (-1) / 3 + (float)N / 3;
+        if ((countFive * (-1) * 5 / 3 + (float)N / 3 - countThree) == (float)0)
         {
+            check = 0;
             break;
         }
-        else
-        {
-            count--;
-            N += 5;
-        }
+        check = 1;
+    }
+    if (check == 0)
+    {
+        printf("%1.0f", countFive + countThree);
+    }
+    else
+    {
+        printf("%d", -1);
     }
 }
+
+/**
+ *  임의의 수 N 에 대하여, N=5*x+3*y 에서 x+y 가 최소값인 경우를 찾는다. 해가 없다면 -1 출력.
+ */
