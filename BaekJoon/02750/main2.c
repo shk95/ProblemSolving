336bd5aaf28e474a13d64721a1e516a3c1bfda17
user123145 <shk9510@gmail.com>
2022-10-21 21:32:26 +0900

    정답 정렬 2 버블정렬

---
+++ BaekJoon/02750/main2.c
@@ -0,0 +1,39 @@
+/*
+거품정렬
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int *pArr = malloc(sizeof(int) * N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int t;
+        scanf("%d", &t);
+        pArr[i] = t;
+    }
+
+    //정렬. 큰수가 뒤로.
+    for (int i = 0, tmp = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - 1 - i; j++)
+        {
+            if (pArr[j] > pArr[j + 1])
+            {
+                tmp = pArr[j + 1];
+                pArr[j + 1] = pArr[j];
+                pArr[j] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d\n", pArr[i]);
+    }
+}
