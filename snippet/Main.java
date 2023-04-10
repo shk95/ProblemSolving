e824994fcf13f8ac1f9be99970b5f70f1079c36f
sh-kang <shk9510@gmail.com>
2023-04-10 04:38:11 +0000

    snippet

--- snippet/Main.java
+++ snippet/Main.java
@@ -1,9 +1,17 @@
 import java.util.*;
+import java.io.*;
 
 class Main{
-	public static void main(String... args){
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		System.out.println(s);
+	public static void main(String... args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = bf.readLine();
+		int ii = Integer.parseInt(bf.readLine());
+
+
+		bw.write();
+		bw.flush();
+		bw.close();
 	}
 }
