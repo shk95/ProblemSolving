dab4a535ccb77f838a731e5ae09aec0c27967548
sh-kang <shk9510@gmail.com>
2023-04-10 04:40:10 +0000

    temp

---
+++ BaekJoon/1934.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	public static void main(String... args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int ii = Integer.parseInt(bf.readLine());
+		for (int i = 0; i < ii ; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+
+		bw.write();
+		
+		bw.flush();
+		bw.close();
+	}
+}
